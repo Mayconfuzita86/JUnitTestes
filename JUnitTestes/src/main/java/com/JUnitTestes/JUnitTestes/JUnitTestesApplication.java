ed241a61ec02a564ba9bc4744790a2df673af848
Mayconfuzita86 <mayconfuzita@gmail.com>
2024-02-07 15:10:49 -0300

    Testes com JUnit

---
+++ JUnitTestes/src/main/java/com/JUnitTestes/JUnitTestes/JUnitTestesApplication.java
@@ -0,0 +1,13 @@
+package com.JUnitTestes.JUnitTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnitTestesApplication.class, args);
+	}
+
+}
---
+++ JUnitTestes/src/test/java/com/JUnitTestes/JUnitTestes/JUnitTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JUnitTestes.JUnitTestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
